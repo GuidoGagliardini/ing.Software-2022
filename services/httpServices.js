28b216a8d9944cdd5f44de196af46ce04a376523
guido <guido@blockinar.com>
2022-12-23 14:01:30 -0300

    fix services

--- services/httpServices.js
+++ services/httpServices.js
@@ -1,12 +1,31 @@
 var express = require('express');
 var axios  = require ('axios');
+const PokeModel =  require('../models/pokeModel');
 var router = express.Router();
 
 
 
 const dataPoke = async ()=>{
-    const result = await axios.default.get('https://pokeapi.co/api/v2/pokemon/');
-    return result;
+    try {
+        const result = await axios.default.get('https://pokeapi.co/api/v2/pokemon/');
+        let arraySplice = result.data.results.slice(-5);
+        arraySplice.forEach(async i => {
+            try {
+                let pokeJson ={
+                    name: i.name,
+                    type:"Tierra"
+                }
+                let saveData = await PokeModel.create(pokeJson);
+            } catch (error) {
+                console.log(error);
+                return error;
+            }
+        })
+
+        return result;
+    } catch (error) {
+        return error;
+    }
 }
 const dataPokeId = async (id)=>{
     const result = await axios.default.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
@@ -18,4 +37,8 @@ const updatePoke = async (id,data)=>{
     const result = await axios.default.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
     return result;
 }
-module.exports = {dataPoke,dataPokeId,updatePoke};
+const getPokesSave =  async ()=>{
+    const dataPoke = await PokeModel.findAll();
+    return dataPoke;
+}
+module.exports = {dataPoke,dataPokeId,updatePoke,getPokesSave};
--- services/userServces.js
+++ services/userServces.js
@@ -4,6 +4,9 @@ var express = require('express');
 var axios  = require ('axios');
 var router = express.Router();
 const UsersModel =  require('../models/usersModel');
+const PokeModel =  require('../models/pokeModel');
+const { associations } = require('../models/usersModel');
+
 
 
 
@@ -11,25 +14,23 @@ const getUser =async (email)=>{
     const dataUser =  await UsersModel.findOne({
         where: {email}
     })
-
-    console.log(dataUser)
 }
 const getAllUsers =  async ()=>{
     const dataUser = await UsersModel.findAll();
-    // return {...dataUser}
-    console.log(dataUser)
     return dataUser
 }
 const getUserById =  async (id)=>{
     const user = await UsersModel.findAll({
         where: {id}
     });
-    console.log(user)
     return user;
 }
-const createUsers = async (name,email)=>{
+const createUsers = async (name,email,id_poke)=>{
    try {
-    const userJson = {name : name ,email : email};
+    const userJson =
+     {  name : name ,
+        email : email,
+        id_poke};
     const post = await UsersModel.create(userJson);
     return post;
    } catch (error) {
@@ -42,7 +43,6 @@ const deleteUser = async(id)=>{
         const deleteData = await UsersModel.destroy({
             where:{id}
         })
-        console.log(deleteData)
         return deleteData;
     } catch (error) {
         return error;
@@ -51,7 +51,6 @@ const deleteUser = async(id)=>{
 
 const updateUser = async (id,updateData)=>{
     try {
-        console.log(updateData)
         const updateAction = await UsersModel.upsert(
                 {
                     id: id,
@@ -65,5 +64,16 @@ const updateUser = async (id,updateData)=>{
         
     }
 }
+const userWhitPoke = async (id)=>{
+    try {
+        const usersWhitPoke = await PokeModel.findByPk(id,{
+            include:[
+               { model: PokeModel, required:false}
+            ]
+        })
+    } catch (error) {
+        
+    }
+}
 
-module.exports={getUser,createUsers,getAllUsers,deleteUser,updateUser,getUserById};
+module.exports={getUser,createUsers,getAllUsers,deleteUser,updateUser,getUserById,userWhitPoke};
